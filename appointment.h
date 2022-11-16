19f9ad75c4f1d5d10e8beec5135cbed57e4c4cd6
DakshinVamsi <dakshinvamsi@gmail.com>
2022-11-16 22:39:49 +0530

    Update appointment.h

--- appointment.h
+++ appointment.h
@@ -1,47 +1,45 @@
 // Appointment system
 
 
-vector<bool> Slots() ;
+vector<bool> Slots(n,1)  ;
 
 //Interface to be written in main file
 
 
 void NewAppointment(patient* x,doctor* head)  {
     doctor* temp = head ;
+    int choice ;
 
     string Doctor ;
     cout << "Enter doctor's name: " ;
     cin >> Doctor ;
 
-    while(temp != NULL) {
+    while(temp != NULL) { 
         if(strcmp(temp->name,Doctor)) break ;
         temp = temp->next ;
-
     }
-
-
-
-    
-
-
-    //Check doctor's available hours and create a bool vector of (end time - start time)/30 mins slots
-    //If the slot reads 0 -> BOOKED ; 1 -> AVAILABLE and display that time slot
 
 
     cout << "Available time slots: " << endl ;
-    for(int i=0 ; i<slots.size() ;i++) {
-        if(temp->Slots[i] == 1) cout << hrs + (i-1)/2 << ':' << mins 
+    for(int i=0 ; i<Slots.size() ;i++) {
+        if(temp->Slots[i] == 1) cout << i+1<< ". " <<  i/3 << ':' << (i%3)*20 << " to " <<  (i+1)/3 << ":" << ((i+1)%3)*20  << endl ;
+
     }
 
-    cout << "Choose slots" 
+    cout << "Choose slot:"  ;
+    cin >> choice ; 
 
-
-
-
+    temp->Slots[choice-1] = 0 ; //Slot gets booked
+ 
     
 }
 
 int waiting_time(vector<bool> slots) {
+    int time;
+
+    for(int i=0;i<Slots.size();i++) {
+        if(i == 0) 
+    }
     return 
 
 
